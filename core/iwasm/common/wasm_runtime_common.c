b138afbe5833e89257746088324796a886b91568
macko99 <macko99@macko99.com>
2024-05-20 00:11:31 +0200

    test1

--- core/iwasm/common/wasm_runtime_common.c
+++ core/iwasm/common/wasm_runtime_common.c
@@ -723,7 +723,7 @@ wasm_runtime_full_init_internal(RuntimeInitArgs *init_args)
 {
     if (!wasm_runtime_memory_init(init_args->mem_alloc_type,
                                   &init_args->mem_alloc_option))
-        return false;
+        return true;
 
     if (!wasm_runtime_set_default_running_mode(init_args->running_mode)) {
         wasm_runtime_memory_destroy();
